4a84bcc7c06ac4fdf3c6394d33b915cdc70409b6
Kumail Kermalli <kumail.kermalli.16@ucl.ac.uk>
2020-11-05 11:40:24 +0000

    added edge test cases and fixed bugs. Answers UCL-RITS/rse-classwork-2020#86

--- week05-testing/test_times.py
+++ week05-testing/test_times.py
@@ -7,3 +7,24 @@ def test_given_input():
     result = (compute_overlap_time(large, short))
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_given_no_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 13:00:00", "2010-01-12 14:00:00")
+    result = (compute_overlap_time(large, short))
+    expected = []
+    assert result == expected
+
+def test_given_multiple_range():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2,60)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2,60)
+    result = (compute_overlap_time(large, short))
+    expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
+    assert result == expected
+
+def test_boundary_overlap():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 13:00:00")
+    result = (compute_overlap_time(large, short))
+    expected = [('2010-01-12 12:00:00')]
+    assert result == expected
--- week05-testing/times.py
+++ week05-testing/times.py
@@ -17,7 +17,10 @@ def compute_overlap_time(range1, range2):
         for start2, end2 in range2:
             low = max(start1, start2)
             high = min(end1, end2)
-            overlap_time.append((low, high))
+            if high > low:
+                overlap_time.append((low, high))
+            elif high == low:
+                overlap_time.append((low))
     return overlap_time
 
 if __name__ == "__main__":
